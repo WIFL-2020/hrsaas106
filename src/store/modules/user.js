e324c519fc844d56837d429301758724ad32c408
梦丶依然在 <1950058059@qq.com>
2021-02-25 20:43:49 +0800

    封装Vuex的登录Action并处理token完成

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,23 +1,31 @@
 import { getToken, setToken, removeToken } from '@/utils/auth'
 import { login } from '@/api/user'
+// 状态
+// 初始化的时候从缓存中读取状态 并赋值到初始化的状态上
+// Vuex的持久化 如何实现 ？ Vuex和前端缓存相结合
 const state = () => ({
   token: getToken() // 设置token初始化  token持久化=》 放到缓存中
 })
+// 修改状态
 const mutations = {
+  // 设置token
   setToken(state, token) {
     state.token = token // 设置token只是修改token的数据
     setToken(token) // vuex和 缓存数据的同步
   },
+  // 删除缓存
   removeToken(state) {
     state.token = null // 删除vuex中的token
     removeToken() // 先清除vuex再清除vuex和缓存数据的同步
   }
 }
+// 执行异步
 const actions = {
   async login(context, data) {
     const result = await login(data)
     // axios会默认为数据加一层data
     if (result.data.success) {
+      // actions 修改state 必须通过mutations
       context.commit('setToken', result.data.data)
     }
   }
@@ -28,100 +36,3 @@ export default {
   mutations,
   actions
 }
-// import { login, logout, getInfo } from '@/api/user'
-// import { getToken, setToken, removeToken } from '@/utils/auth'
-// import { resetRouter } from '@/router'
-
-// const getDefaultState = () => {
-//   return {
-//     token: getToken(),
-//     name: '',
-//     avatar: ''
-//   }
-// }
-
-// const state = getDefaultState()
-
-// const mutations = {
-//   RESET_STATE: (state) => {
-//     Object.assign(state, getDefaultState())
-//   },
-//   SET_TOKEN: (state, token) => {
-//     state.token = token
-//   },
-//   SET_NAME: (state, name) => {
-//     state.name = name
-//   },
-//   SET_AVATAR: (state, avatar) => {
-//     state.avatar = avatar
-//   }
-// }
-
-// const actions = {
-//   // user login
-//   login({ commit }, userInfo) {
-//     const { username, password } = userInfo
-//     return new Promise((resolve, reject) => {
-//       login({ username: username.trim(), password: password }).then(response => {
-//         const { data } = response
-//         commit('SET_TOKEN', data.token)
-//         setToken(data.token)
-//         resolve()
-//       }).catch(error => {
-//         reject(error)
-//       })
-//     })
-//   },
-
-//   // get user info
-//   getInfo({ commit, state }) {
-//     return new Promise((resolve, reject) => {
-//       getInfo(state.token).then(response => {
-//         const { data } = response
-
-//         if (!data) {
-//           return reject('Verification failed, please Login again.')
-//         }
-
-//         const { name, avatar } = data
-
-//         commit('SET_NAME', name)
-//         commit('SET_AVATAR', avatar)
-//         resolve(data)
-//       }).catch(error => {
-//         reject(error)
-//       })
-//     })
-//   },
-
-//   // user logout
-//   logout({ commit, state }) {
-//     return new Promise((resolve, reject) => {
-//       logout(state.token).then(() => {
-//         removeToken() // must remove  token  first
-//         resetRouter()
-//         commit('RESET_STATE')
-//         resolve()
-//       }).catch(error => {
-//         reject(error)
-//       })
-//     })
-//   },
-
-//   // remove token
-//   resetToken({ commit }) {
-//     return new Promise(resolve => {
-//       removeToken() // must remove  token  first
-//       commit('RESET_STATE')
-//       resolve()
-//     })
-//   }
-// }
-
-// export default {
-//   namespaced: true,
-//   state,
-//   mutations,
-//   actions
-// }
-
--- src/utils/auth.js
+++ src/utils/auth.js
@@ -1,6 +1,6 @@
 import Cookies from 'js-cookie'
 
-const TokenKey = 'hrsaas-ihrm-token'
+const TokenKey = 'hrsaas-ihrm-token' // 设定一个独一无二的key
 
 export function getToken() {
   return Cookies.get(TokenKey)
